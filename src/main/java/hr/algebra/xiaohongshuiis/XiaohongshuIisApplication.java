0698c1870deadea9a730ce10e03657a3c226e700
Bruno Ravenščak <bravens@algebra.hr>
2025-06-24 23:35:28 +0200

    Initial commit

---
+++ src/main/java/hr/algebra/xiaohongshuiis/XiaohongshuIisApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.xiaohongshuiis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuIisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuIisApplication.class, args);
+    }
+
+}
